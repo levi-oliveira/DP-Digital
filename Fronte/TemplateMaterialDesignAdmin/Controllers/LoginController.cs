c1dd97b188338bc3cf8e8155e953b92f16d9883d
Lucas Santos <lucas.santos@otacom.com.br>
2020-12-04 15:44:19 -0300

    Pasta Fonte

---
+++ Fronte/TemplateMaterialDesignAdmin/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemplateMaterialDesignAdmin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: LoginController/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Logout
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Index", "Login");
+        }
+
+        // GET: LoginController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/EsqueciSenha
+        public ActionResult EsqueciSenha()
+        {
+            return View();
+        }
+
+        // POST: LoginController/EsqueciSenha
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EsqueciSenha(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Fronte/TemplateMaterialDesignAdmin/Models/Comum/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TemplateMaterialDesignAdmin.Models.Comum
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
