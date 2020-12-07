bb8f4ace91e56deed6ce0bbb8f23edb288a68095
Lucas Santos <lucas.santos@otacom.com.br>
2020-12-07 08:50:49 -0300

    Iniciando appsettings

---
+++ Fronte/TemplateMaterialDesignAdmin/Settings.cs
@@ -0,0 +1,19 @@
+﻿namespace TemplateMaterialDesignAdmin
+{
+    public class Settings
+    {
+        public string DPDIGITAL_API_CADIDATO { get; set; }
+        public string DPDIGITAL_API_COLABORADOR { get; set; }
+
+        public string ENDPOINT_CANDIDATO_OBTERTODOS { get; set; }
+        public string ENDPOINT_CANDIDATO_OBTERPORID { get; set; }
+        public string ENDPOINT_CANDIDATO_OBTERPORNOME { get; set; }
+        public string ENDPOINT_CANDIDATO_INSERIR { get; set; }
+        public string ENDPOINT_CANDIDATO_ALTERAR { get; set; }
+        public string ENDPOINT_CANDIDATO_REMOVER { get; set; }
+
+        public string ENDPOINT_COLABORADOR_OBTERPORID { get; set; }
+        public string ENDPOINT_COLABORADOR_OBTERPORNOME { get; set; }
+        public string ENDPOINT_COLABORADOR_INSERIR { get; set; }
+    }
+}
